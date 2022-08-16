d02e0592153e2ac2e54c458b32dfe7be254ee73f
Shuta4 <skirdin.nik@gmail.com>
2022-08-16 23:58:26 +0300

    Change font size

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_norm_fg[]     = "#ffffff";
 static const char col_norm_bg[]     = "#121212";
 static const char col_norm_br[]     = "#121212";
